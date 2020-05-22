5d7666ba497fba93bf238d8aabefde91f4d82433
Alissa Brown <abrown324@gmail.com>
2020-05-22 15:24:33 -0500

    added plots and code to read in glacier shp files

--- glaciers.R
+++ glaciers.R
@@ -4,6 +4,13 @@ require(raster)
 require(dplyr)
 require(rgeos)
 require(ggplot2)
+require(gridExtra)
+
+# require(geosphere)
+# require(sp)
+
+# read in pollen data
+pollen <- readRDS("juglans_data.RData")
 
 # projection you want to use
 proj_out <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5
@@ -18,12 +25,13 @@ na_shp <- readOGR("shapefiles/NA_States_Provinces_Albers.shp", "NA_States_Provin
 na_shp <- sp::spTransform(na_shp, proj_out)
 cont_shp <- subset(na_shp,
                    (NAME_0 %in% c("United States of America", "Mexico", "Canada")))
+
+# START WITH 1K YBP
 # glacier shapefile (start with 1k YBP to present)
 shp_1k <- readOGR("ice/1ka_0.91cal_Dalton_et_al_2020_QSR.shp", "1ka_0.91cal_Dalton_et_al_2020_QSR")
 shp_1k <- sp::spTransform(shp_1k, proj_out)
 
-# read in pollen data, select observations from 1k YBP - present
-pollen <- readRDS("juglans_data.RData")
+# select pollen observations from 1k YBP - present
 pollen_1k <- pollen %>% filter(age <= 1000) %>% select(-juglans.nigra, -cut)
 pollen_1k <- pollen_1k %>% filter(juglans.cinerea >0, !is.na(juglans.cinerea))
 
@@ -57,4 +65,65 @@ ggplot() +
         plot.title = element_blank()) +
   coord_fixed()
 
+
+
+# PERFORM PREVIOUS STEPS ON ALL DATA IN 1000-YEAR TIME CHUNKS
+
+# cut data into time chunks (if it doesn't already have a 'cut' column)
+# dat.cut <- cut(pollen$age, breaks = c(min(pollen$age, na.rm = TRUE), seq(1000, 21000, by = 1000)),
+#                labels = FALSE)
+# pollen$cut <- dat.cut
+# pollen <- pollen[!is.na(pollen$cut),]
+
+# separate dataframe into list of dataframes, one for each time chunk
+# only use j. cinerea
+time <- split(pollen, f = pollen$cut)
+cinerea_list <- lapply(time, "[", c(4, 7:9))
+
+# remove sites that contain NA or 0 pollen counts for each time chunk
+  for(i in 1:21){
+    cinerea_list[[i]] <- subset(cinerea_list[[i]], 
+                                 !is.na(cinerea_list[[i]][,"juglans.cinerea"]) & 
+                                  cinerea_list[[i]][,"juglans.cinerea"] != 0)
+  }
+
+# sum counts when there are multiple counts at a site during each time period
+# (need one count per site per time period)
+agg_list <- list()
+
+for(i in 1:21){
+agg_list[[i]] <- cinerea_list[[i]] %>% group_by(x, y) %>% 
+  summarise(juglans.cinerea = sum(juglans.cinerea))
+agg_list[[i]] <- as.data.frame(agg_list[[i]])
+}
+
+
+# PLOTTING LOOPS OVER TIME INTERVALS, CIRCLES FILLED BY RELATIVE ABUNDANCE
+plot_list <- list(list())
+
+  for(i in 1:length(agg_list)){
+    plot_list[[i]] <- ggplot(data = agg_list[[i]]) +
+      geom_point(aes_string(x = 'x', y = 'y', fill = 'juglans.cinerea'), 
+                 size = 4, pch = 21, alpha = 0.5, color = 'red') +
+      geom_path(data = cont_shp, aes(x = long, y = lat, group = group), 
+                alpha = 0.5) +
+      labs(subtitle = paste0(i, "000")) +
+      scale_y_continuous(limits = c(-1500000, 4000000)) +
+      scale_x_continuous(limits = c(-1000000, 3000000)) +
+      theme_classic() +
+      theme(axis.ticks = element_blank(),
+            axis.text = element_blank(),
+            axis.title = element_blank(),
+            line = element_blank(),
+            legend.position = 'none') +
+      coord_fixed()
+  }
+
+
+# READ IN GLACIER SHAPEFILES AND ADD THEM TO PLOTS BY TIME CHUNK
+for(i in 1:21){
+ice <- readOGR("ice/1ka_0.91cal_Dalton_et_al_2020_QSR.shp", 
+                  "1ka_0.91cal_Dalton_et_al_2020_QSR")
+ice <- sp::spTransform(shp_1k, proj_out)
+}
 
